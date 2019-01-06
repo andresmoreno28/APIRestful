e94441dcbf58a7f233676f01f15b550fb3d20955
andresmoreno <andresmrubio28@gmail.com>
2019-01-07 00:48:07 +0100

    Clase 32, Sección 7 (commit)
    Se ha modificado el  fichero de rutas para API añadiendo los end-points primarios necesarios para la API Restful

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,40 @@ use Illuminate\Http\Request;
 |
 */
 
+/*Args('Nombre del recurso en plural', 'Nombre o ruta del controlador', [filtro de métodos])*/
+
+/*
+ * Buyers
+ */
+Route::resource('buyers', 'Buyer\BuyerController', ['only' => ['index', 'show']]);
+
+/*
+ * Categories
+ */
+Route::resource('categories', 'Category\CategoryController', ['except' => ['create', 'edit']]);
+
+/*
+ * Products
+ */
+Route::resource('products', 'Product\ProductController', ['only' => ['index', 'show']]);
+
+/*
+ * Transactions
+ */
+Route::resource('transactions', 'Transaction\TransactionController', ['only' => ['index', 'show']]);
+
+/*
+ * Sellers
+ */
+Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show']]);
+
+/*
+ * Users
+ */
+Route::resource('users', 'User\UserController', ['except' => ['create', 'edit']]);
+
+
+
 /*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
